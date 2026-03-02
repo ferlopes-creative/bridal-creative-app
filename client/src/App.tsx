083e59867c7f11a44f017c6859e65199565e30de
Fer Lopes <ferlopes.contato@gmail.com>
2026-03-02 04:04:34 -0300

    Remove layout wrapper and adjust responsive container

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,44 +1,16 @@
-import { Toaster } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/NotFound";
-import { Route, Switch, Redirect } from "wouter";
-import ErrorBoundary from "./components/ErrorBoundary";
-import { ThemeProvider } from "./contexts/ThemeContext";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import Community from "./pages/Community";
-import Layout from "./components/Layout";
-
-function Router() {
-  return (
-    <Switch>
-      <Route path="/">
-        <Redirect to="/login" />
-      </Route>
-
-      <Route path="/login" component={Login} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/community" component={Community} />
-
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
-
 function App() {
   return (
     <ErrorBoundary>
       <ThemeProvider defaultTheme="light">
         <TooltipProvider>
-          <Layout>
-            <Router />
-          </Layout>
-
-          <Toaster />
+          <div className="min-h-screen w-full bg-background">
+            <div className="w-full max-w-6xl mx-auto px-4 md:px-10">
+              <Router />
+            </div>
+            <Toaster />
+          </div>
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
   );
 }
-
-export default App;
