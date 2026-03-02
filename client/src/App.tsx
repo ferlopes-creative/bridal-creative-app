14b424ec1d50ddf27b3a8fbe7cc6a7d17358526f
Fer Lopes <ferlopes.contato@gmail.com>
2026-03-02 04:18:35 -0300

    Fix App imports and responsive layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,36 @@
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/NotFound";
+import { Route, Switch, Redirect } from "wouter";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Community from "./pages/Community";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/">
+        <Redirect to="/login" />
+      </Route>
+      <Route path="/login" component={Login} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/community" component={Community} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
 function App() {
   return (
     <ErrorBoundary>
       <ThemeProvider defaultTheme="light">
         <TooltipProvider>
           <div className="min-h-screen w-full bg-background">
-            
-            {/* Layout Responsivo Global */}
-            <div className="
-              w-full 
-              mx-auto 
-              px-4 
-              md:px-8 
-              lg:px-16 
-              xl:px-24
-            ">
+            <div className="w-full mx-auto px-4 md:px-8 lg:px-16 xl:px-24">
               <Router />
             </div>
-
             <Toaster />
           </div>
         </TooltipProvider>
@@ -24,3 +38,5 @@ function App() {
     </ErrorBoundary>
   );
 }
+
+export default App;
